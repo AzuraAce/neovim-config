086f2babc865dfb933dd8d78da8b79940ed04497
AzuraAce <oliverjbmub@gmail.com>
2025-03-28 17:04:56 +0100

    added manual folding; replaced nnn-nvim with nnn.vim

--- flake.nix
+++ flake.nix
@@ -153,7 +153,6 @@
             oil-nvim
             nvim-web-devicons
             image-nvim
-            luasnip
             nnn-vim
           ];
           texPlugins = [
--- lua/myLuaConf/opts_and_keys.lua
+++ lua/myLuaConf/opts_and_keys.lua
@@ -2,13 +2,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
+vim.g.foldmethod='manual'
 
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
